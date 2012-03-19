2f709bebaca2550f16689a5f429a4d56f9bfa311
milui.galithil@gmail.com <milui.galithil@gmail.com@f5842eb1-31af-45c0-88a4-f11e6906b4e3>
2012-03-19 22:44:10 +0000

    adding the char simulator base

---
+++ wod-char-simulator/src/Creature.java
@@ -0,0 +1,66 @@
+
+public abstract class Creature {
+
+	public int initD;
+	public int attackD;
+	public int damageD;
+	public int dodgeD;
+	public int resistanceD;
+	public int rAggD;
+	public Levels lev;
+	
+	private int bonusfromattack=0;
+	
+	public Creature(int initD, int attackD, int damageD, int dodgeD,
+			int resistanceD, int rAggD, Levels lev) {
+		super();
+		this.initD = initD;
+		this.attackD = attackD;
+		this.damageD = damageD;
+		this.dodgeD = dodgeD;
+		this.resistanceD = resistanceD;
+		this.rAggD=rAggD;
+		this.lev=lev;
+	}
+	
+	public int initRoll(){
+		
+		DiceManager dm=new DiceManager();
+		return(dm.realHandler(initD+lev.initM, 10, 4, lev.rerolltens, true));
+	}
+	public int attackRoll(){
+		
+		DiceManager dm=new DiceManager();
+		int successes=dm.realHandler(attackD+lev.attackM, 10, 7, lev.rerolltens, true);
+		if(successes>=0){
+			bonusfromattack=successes;
+		}else{
+			
+			bonusfromattack=0;
+		}
+		return(successes);
+	}
+	public int damageRoll(){
+		
+		DiceManager dm=new DiceManager();
+		return(dm.realHandler(damageD+lev.damageM+bonusfromattack, 10, 7, false, false));
+	}
+	
+	public int dodgeRoll(){
+		
+		DiceManager dm=new DiceManager();
+		return(dm.realHandler(dodgeD+lev.dodgeM, 10, 7, lev.rerolltens, true));
+	}
+	public int resistanceRoll(){
+		
+		DiceManager dm=new DiceManager();
+		return(dm.realHandler(resistanceD+lev.resistanceM, 10, 7, lev.rerolltens, true));
+		
+	}
+	public int aggravatedRoll(){
+		
+		DiceManager dm=new DiceManager();
+		return(dm.realHandler(rAggD+lev.rAggM, 10, 7, lev.rerolltens, true));
+		
+	}
+}
---
+++ wod-char-simulator/src/Strong.java
@@ -0,0 +1,16 @@
+
+public class Strong extends Levels {
+
+	//currently at +3
+	public Strong(){
+		super.initM=3;
+		super.attackM=3;
+		super.damageM=3;
+		super.dodgeM=3;
+		super.resistanceM=3;
+		super.rAggM=3;
+		super.rerolltens=true;
+		
+		
+	}
+}

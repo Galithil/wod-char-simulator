07699f5ff701b2bd2ba16a02b2f22dbab20e28f6
milui.galithil@gmail.com <milui.galithil@gmail.com@f5842eb1-31af-45c0-88a4-f11e6906b4e3>
2012-03-25 19:53:08 +0000

    correcting Goule

--- trunk/wod-char-simulator/src/Vampire.java
+++ trunk/wod-char-simulator/src/Vampire.java
@@ -1,8 +1,12 @@
 
 public class Vampire extends Creature {
 	
-	public Vampire(Levels lev){
-		super(7,8,6,6,6,3, lev);
+	public Vampire(String name,Levels lev){
+		super(name,7,8,6,6,6,3, lev);
 		
 	}
+    @Override
+         public String toString(){
+         return (this.name+"   Vampire   "+this.lev.toString());
+         }
 }

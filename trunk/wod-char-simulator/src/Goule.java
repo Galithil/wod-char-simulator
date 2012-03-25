dc48af04b987072562da2fd51562a7c059764bee
milui.galithil@gmail.com <milui.galithil@gmail.com@f5842eb1-31af-45c0-88a4-f11e6906b4e3>
2012-03-25 19:52:02 +0000

    correcting Goule

--- trunk/wod-char-simulator/src/Goule.java
+++ trunk/wod-char-simulator/src/Goule.java
@@ -1,8 +1,11 @@
 
 public class Goule extends Creature {
 
-	public Goule(Levels lev){
-		super(5,7,4,6,6,1, lev);
+	public Goule(String name,Levels lev){
+		super(name,5,7,4,6,6,1, lev);
 		
 	}
+         public String toString(){
+         return (this.name+"   Goule   "+this.lev.toString());
+         }
 }
